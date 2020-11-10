fd849ecaf9964083c3a669eb165f7a7b8f44e1d5
Federico Izaguirre <fedej.izaguirre@gmail.com>
2020-11-10 12:50:58 +0100

    Set up index function
    the app tells if it's friday or not, based on the internal system clock.

--- vrijdag/views.py
+++ vrijdag/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from datetime import datetime
 
 # Create your views here.
+
+
+def index(request):
+    now = datetime.today()
+    day = now.weekday()
+    return render(request, "vrijdag/index.html", {"vrijdag": day == 4})
